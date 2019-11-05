b9ff90c449e408f4f8775e4c2b9406de9c599720
Dear <>
2019-11-05 08:13:11 +0700

    Initial Project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <WinterScene/>
+  </div>
+</template>
+
+<script>
+import WinterScene from './views/WinterScene.vue'
+
+export default {
+  name: 'app',
+  components: {
+    WinterScene
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/WinterScene/Light2.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <g id="light-2-2" data-name="light-2">
+            <circle id="light-4" data-name="light" class="cls-22" cx="666.11" cy="410.38" r="42.81" />
+            <circle id="light-5" data-name="light" class="cls-23" cx="540.84" cy="505.86" r="42.81" />
+            <circle id="light-6" data-name="light" class="cls-24" cx="790.39" cy="545.58" r="42.81" />
+        </g>
+    </div>
+</template>
